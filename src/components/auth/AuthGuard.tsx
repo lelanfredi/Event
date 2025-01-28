21cffa5fabb0781105fb162a01390098b307d602
lelanfredi <leticialanfredi@gmail.com>
2025-01-28 01:26:39 +0000

    authguard

--- src/components/auth/AuthGuard.tsx
+++ src/components/auth/AuthGuard.tsx
@@ -2,7 +2,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { ReactNode } from "react";
 import AuthDialog from "./AuthDialog";
 
-export function AuthGuard({ children }: { children: ReactNode }) {
+type AuthGuardProps = {
+  children: ReactNode;
+};
+
+export function AuthGuard({ children }: AuthGuardProps) {
   const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
 
   if (isLoading) {
